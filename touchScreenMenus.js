cfe34a029451daeb428766dbbec61088c6df180b
Greg Harvell <greg@blueacorn.com>
2012-12-11 22:05:54 -0500

    Rewrote touchScreenMenus, added comments to assist developers.
    Only did a few quick tests, hopefully github users tell me how bad my code is.

--- touchScreenMenus.js
+++ touchScreenMenus.js
@@ -1,18 +1,58 @@
-Event.observe(window, 'load', function(){
-	   var el = document.createElement('div');
-	   var regClick = 0;
-	   el.setAttribute('ongesturestart', 'return;');
-	   var navEl = $$('#nav>li>a');
-	   if(typeof el.ongesturestart == "function"){
-	   	  navEl.invoke('observe', 'click', function(navClick){
-	   	      
-	   	      if(regClick == 0){
-	   	          Event.stop(navClick);
-	   	          regClick = 1;
-	   	      }else{
-	   	          regClick = 0;
-	   	      }
-	   	      
-	   	  });
-	   }
+var touchMageMenu;
+
+var touchMagentoMenus = Class.create({
+
+    initialize: function () {
+        this.navElement = $$('#nav>li>a');
+
+        if(this.isTouchScreen){
+            this.touchNavigation();
+        }
+
+    },
+
+    touchNavigation: function(){
+        var currentMenuItem, newMenuItem;
+        currentMenuItem = {
+            anchor: false
+        }
+        this.navElement.invoke('observe', 'click', function(evt){
+            newMenuItem = evt.findElement('a');
+            if(currentMenuItem.anchor){
+                if(currentMenuItem.anchor !== newMenuItem){
+                    Event.stop(evt);
+                    currentMenuItem.anchor = newMenuItem;
+                }
+            }else{
+                Event.stop(evt);
+                currentMenuItem.anchor = newMenuItem;
+            }
+        });
+    },
+
+    isTouchScreen: function(){
+
+        /*
+         *  Function to determine if Browser is Safari Mobile (iOS) or WebKit Mobile (Android).
+         *  Still need to create Firefox Mobile Functionality
+         */
+
+        if(Prototype.Browser.MobileSafari || (Prototype.Browser.WebKit && function(){
+            try{
+                document.createEvent('TouchEvent');
+                return true;
+            } catch(e) {
+                return false;
+            }
+        })){
+            return true;
+        }
+    }
+
 });
+
+Event.observe(window, 'load', function () {
+
+    touchMageMenu = new touchMagentoMenus();
+
+}.bind(window));
